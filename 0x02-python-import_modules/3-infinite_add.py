8a6876d1764912a981cdc576124e48d3231a4335
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-01 21:09:58 +0000

    add infinitely

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    '''Number of arguments excluding argv[0]'''
+    count = len(argv) - 1
+    total = 0
+
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+    print(total)
