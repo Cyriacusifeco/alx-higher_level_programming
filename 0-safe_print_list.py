600b1f45acc787b454cbdd59f194bb14fac2d987
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-20 00:07:55 +0000

    print list

---
+++ 0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    count = 0
+
+    while i < x:
+        try:
+            print(my_list[i], end='')
+            count = count + 1
+
+        except IndexError:
+            return count
+
+        i = i + 1
+    print('\n')
+    return count
