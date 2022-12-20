911b14b508c699a093401d3f6afae076a18b41a4
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-20 02:06:18 +0000

    print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a/b
+        return result
+
+    except TypeError:
+        return None
+    except ValueError:
+        return None
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {:f}".format(result))
+
+    print()
+
