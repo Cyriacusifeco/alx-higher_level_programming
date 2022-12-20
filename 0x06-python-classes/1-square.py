af8483012adf665b7f639814f3c5da8d69b93f9f
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-20 22:06:20 +0000

    Add private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Classes and objects are the two main aspects of object oriented programming.
+A class creates a new type where objects are instances of the class. In this
+module, we attempt to build our Class "Square" from the ground up step by step.
+
+"""
+
+
+class Square:
+
+    """
+    Next, we create a private instance attribute: size with no capabilities.
+
+    """
+    def __init__(self, size):
+
+        self._size = size
