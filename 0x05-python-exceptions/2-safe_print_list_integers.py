54f561c888b3325eac1b0fe3eebf3090f97a3bdf
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-20 01:42:30 +0000

    print list items

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    i = 0
+
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+
+            count = count + 1
+
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        except IndexError:
+            print()
+            return count
+
+        i = i + 1
+    
+    print()
+    return count
+
