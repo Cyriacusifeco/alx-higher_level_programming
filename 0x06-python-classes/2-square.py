0577ae4d7da542eedf6ff5f439c1e1bad915d97a
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-20 22:47:29 +0000

    Size Validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Classes and objects are the two main aspects of object oriented programming.
+A class creates a new type where objects are instances of the class. In this
+module, we attempt to build our Class "Square" from the ground up step by step.
+
+"""
+
+
+class Square:
+
+    """
+    Next, we create a private instance attribute: size with no capabilities.
+
+    """
+    def __init__(self, size=0):
+
+        """
+        initializes the attribute; size
+        with no type or value verification.
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
