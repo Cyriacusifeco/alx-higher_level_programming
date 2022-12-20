d604953b81007d1d9411ff1f60016ba2c8284b5a
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-20 21:52:07 +0000

    My First Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,8 +2,7 @@
 """
 Classes and objects are the two main aspects of object oriented programming.
 A class creates a new type where objects are instances of the class. In this
-module, we attempt to build our first Class(type) with "Square" as its instance
-from the ground up step by step.
+module, we attempt to build our Class(type) "Square" from the ground up step by step.
 
 """
 
