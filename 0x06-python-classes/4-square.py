365eae42f61458f239c7ada2ca44b2bda02e4e69
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-21 22:19:50 +0000

    remove repetitions

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,11 +21,6 @@ class Square:
         leaving verification for the setter.
 
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
         self.__size = size
 
