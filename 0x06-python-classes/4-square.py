95c9efa60304bf83115d4e2e4c4b1015e2a1d05c
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-21 00:25:53 +0000

    Access and Update Size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+Classes and objects are the two main aspects of object oriented programming.
+A class creates a new type where objects are instances of the class. In this
+module, we attempt to build our Class "Square" from the ground up step by step.
+
+"""
+
+
+class Square:
+
+    """
+    Next, we build on the private instance attribute:size with capabilities
+    while maintaining the type and value validation. Adding getters & setters.
+
+    """
+    def __init__(self, size=0):
+
+        """
+        initializes the attribute; size while
+        leaving verification for the setter.
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+
+        """
+        A public instance method that returns the square
+        of size.
+
+        """
+
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+
+        """
+        A method that accesses private attribute - size
+
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """
+        Responsible for setting the value of our private attribute: size
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
