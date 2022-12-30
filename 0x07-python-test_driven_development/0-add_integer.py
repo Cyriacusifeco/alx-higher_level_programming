7a43935dd20bccf846a2ac0dbba80ed953c35e44
Cyriacusifeco <cyriacusifeco@gmail.com>
2022-12-30 16:10:36 +0000

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of function to add two integers
+with a .txt test file at the root of the directory that runs all the
+edge cases.
+"""
+
+def add_integer(a, b=98):
+    """
+    A function that adds two integers.
+    """
+
+    try:
+        a = int(a)
+
+    except (TypeError, ValueError):
+
+            return "a must be an integer"
+    try:
+
+        b = int(b)
+
+    except (TypeError, ValueError):
+
+            return "b must be an integer"
+
+    return(a + b)
