e5a511a4133effb550157287e7eb39f9a580909a
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-01 14:07:36 +0000

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,23 +5,21 @@ with a .txt test file at the root of the directory that runs all the
 edge cases.
 """
 
+
 def add_integer(a, b=98):
     """
     A function that adds two integers.
     """
-
     try:
         a = int(a)
 
     except (TypeError, ValueError):
+        return "a must be an integer"
 
-            return "a must be an integer"
     try:
-
         b = int(b)
 
     except (TypeError, ValueError):
-
-            return "b must be an integer"
+        return "b must be an integer"
 
     return(a + b)
