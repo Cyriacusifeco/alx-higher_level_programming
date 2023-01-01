4a437d04cc7304f60bd46ef28fb8cdee828ad209
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-01 13:05:17 +0000

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+
+    if not isinstance(size, int) and not isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if isinstance(size, float):
+        size = int(size)
+
+    for i in range(size):
+        print("#" * size)
