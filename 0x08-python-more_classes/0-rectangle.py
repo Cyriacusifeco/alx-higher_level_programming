34c2bdb9902f9d7206ba705fa323454a37ab8aaf
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-02 17:37:18 +0000

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,7 +2,8 @@
 """
 Classes and objects are the two main aspects of object oriented programming.
 A class creates a new type where objects are instances of the class. In this
-module, we attempt to build another Class "Rectangle" from the ground up step by step.
+module, we attempt to build another Class "Rectangle" from the
+ground up step by step.
 
 """
 
