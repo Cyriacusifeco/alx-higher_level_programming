fb5894c8d4e135988cbf74dc88c6830f9fb0671d
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-02 19:32:52 +0000

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,8 +23,8 @@ class Rectangle:
 
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
