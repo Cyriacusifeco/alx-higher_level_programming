25c1b249fcf9814aa596620cb88d47b0f3786dbf
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-09 16:38:07 +0000

    Check a kind

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""
-module for is_same_class.
-"""
+"""Contains the function is_kind_of_class"""
 
 
-def is_same_class(obj, a_class):
-    """ exactly an instance """
-    return type(obj) == a_class
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of specified class
+    Args:
+        obj: instance of class a_class
+        a_class: class
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
