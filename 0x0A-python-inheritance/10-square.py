445a315daa208a5ccd1c0291811a4ccc09208f00
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-09 16:58:02 +0000

    square it up!

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""initialize module"""
+"""BaseGeometry module"""
+Rectangle = __import__('9-rectangle').Rectangle
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class Square(Rectangle):
+    """square class"""
 
-class Rectangle(BaseGeometry):
-    """create class and inheritance BaseGeometry"""
+    def __init__(self, size):
+        """constructor"""
 
-    def __init__(self, width, height):
-        """instancy"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """create Area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """str"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size)
