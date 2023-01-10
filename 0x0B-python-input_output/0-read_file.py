0bcd91b616a7381f41e85f1bd1e444456eaaa55b
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-10 17:41:15 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,6 @@
 def read_file(filename=""):
     """create function that read a text file"""
     with open(filename, encoding="UTF8") as f:
-
-        print(f.read())
+        line = f.read()
+    print(line, end="")
+    f.close()
