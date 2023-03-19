398eafe44348dbceb55c8622c2d418a883aab895
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-03-19 21:13:36 +0000

    prevent sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Display name argument of states table"""
+import MySQLdb
+from sys import argv
+
+
+def filter__names():
+    """Takes arguments argv to list from database
+    Only lists with states by user input
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         charset="utf8",
+                         )
+
+    # declaring arguments passed, with query.
+    name = argv[4]
+
+    # Getting a cursor in MySQLdb python
+    cur = db.cursor()
+
+    # Executing db queries
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
+                (name,))
+
+    # fetches all the rows of a query result
+    query_rows = cur.fetchall()
+
+    # Printing the result one in one
+    for row in query_rows:
+        if row[1] == name:
+            print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    filter__names()
