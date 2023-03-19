7d69873d693442a3a51c44a4b5352c626610f366
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-03-19 21:47:09 +0000

    order by state i.d

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Prints the first State objects from the db hbtn_0e_6_usa
+Usage: ./8-model_state_fetch_first.py <mysql username>\
+                                    <mysql passwd>\
+                                    <database name>
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
