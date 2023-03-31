456ec77fe93dbdf275f800ee802e5c1abe741d4c
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-03-31 21:48:19 +0000

    fetch status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""initializate"""
-from urllib.request import urlopen
-
-
-def alx_status_0():
-    """function show my status"""
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        page = response.read()
-        utf8 = page.decode('utf-8')
-        print("Body response:\n\t- type: {}".format(type(page)))
-        print("\t- content: {}\n\t- utf8 content: {}".
-              format(page, utf8, end=""))
+""" Fetches a given url using urlib """
+import urllib.request
 
 
 if __name__ == '__main__':
-    alx_status_0()
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+            type(content), content,
+            content.decode('utf-8')))
