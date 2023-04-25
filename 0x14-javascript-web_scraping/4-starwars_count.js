06a7f401e77f8b1e4eb78893bc7cf211769520dd
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-04-25 14:09:01 +0000

    count Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = process.argv[2];
 
-request.get(apiUrl, function (error, response, body) {
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    let countWedge = 0;
+  } else if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
-    for (let i = 0; i < films.length; i++) {
-      const charList = films[i].characters;
-      for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi.co/api/people/18/' || charList[j] === 'http://swapi.co/api/people/18/') {
-          countWedge += 1;
+    let count = 0;
+    for (const filmIdx in films) {
+      const characters = films[filmIdx].characters;
+      for (const charIdx in characters) {
+        if (characters[charIdx].includes('18')) {
+          count++;
         }
       }
     }
-    console.log(countWedge);
+    console.log(count);
+  } else {
+    console.log('An error occured. status coode: ' + response.statusCode);
   }
 });
